2050afc03eb0e5959e63b9d9ca546eeac25bfad5
Angie Kobeleva-Titova <angie@Angies-MacBook-Pro.local>
2016-12-09 10:44:42 +0200

    [Scripts] Collapse all accordion panels but active

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,8 +7,18 @@ const accordionButtons = document.querySelectorAll('.js-accordion-trigger');
 
 [...accordionButtons].forEach(button => {
   button.addEventListener('click', function(event) {
+    collapseOtherPanels(this);
     const newAriaExpanded = !(this.getAttribute('aria-expanded') == 'true');
     this.setAttribute('aria-expanded', newAriaExpanded);
     this.classList.toggle('accordion__trigger--expanded');
   });
 });
+
+
+function collapseOtherPanels(ele) {
+  const otherAccordionButtons = [...accordionButtons].filter(btn => btn !== ele);
+  otherAccordionButtons.forEach(button => {
+    button.setAttribute('aria-expanded', false);
+    button.classList.remove('accordion__trigger--expanded');
+  });
+}
